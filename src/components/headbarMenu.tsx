81e351d0cff8e528d02f2928b8872a7611a6bfd5
Innokenty <memories53@mail.ru>
2025-07-01 07:58:44 +0300

    save

--- src/components/headbarMenu.tsx
+++ src/components/headbarMenu.tsx
@@ -17,7 +17,7 @@ const HeadbarMenu = () => {
         <header className="w-full h-14 flex items-center px-4">
             <div className="flex items-center justify-between w-full px-2">
                 <div className="flex-1 min-w-0">
-                    <Link href="/">
+                    <Link href="/" className="flex w-[32px]">
                         <Image
                             src="/Logo_icon_(illustration)_(32x32).svg"
                             alt="OneSelfJob Logo"
@@ -41,7 +41,7 @@ const HeadbarMenu = () => {
                 <div className="flex-1 flex items-center justify-end">
                     <button
                         className="md:hidden w-8 h-8 justify-center"
-                        onClick={() => {setIsOpen(true)}}
+                        onClick={() => { setIsOpen(true) }}
                         aria-label="Open menu"
                     >
                         ☰
@@ -49,12 +49,12 @@ const HeadbarMenu = () => {
                 </div>
             </div>
 
-            <div className={`fixed top-0 right-0 h-full w-[400px] bg-white transform transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : 'translate-x-full'} z-40`}
+            <div className={`fixed top-0 right-0 h-full min-w-[300px] bg-white transform transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : 'translate-x-full'} z-40`}
             >
                 <div className="flex justify-between items-center p-4">
                     <span className="text-lg font-semibold">OneSelfJob</span>
                     <button
-                        className="text-xl"
+                        className="text-xl px-4"
                         onClick={() => setIsOpen(false)}
                         aria-label="Закрыть меню"
                     >
@@ -62,19 +62,19 @@ const HeadbarMenu = () => {
                     </button>
                 </div>
                 <nav className="flex flex-col p-4 space-y-4">
-                {menuLinks.map((link) => (
-                    <Link key={link.href} href={link.href} onClick={() => setIsOpen(false)}>
-                        <span className="text-gray-800 hover:text-blue-600">{link.label}</span>
-                    </Link>
-                ))}
+                    {menuLinks.map((link) => (
+                        <Link key={link.href} href={link.href} onClick={() => setIsOpen(false)}>
+                            <span className="text-gray-800 hover:text-blue-600">{link.label}</span>
+                        </Link>
+                    ))}
                 </nav>
             </div>
 
             {/* Backdrop */}
             {isOpen && (
                 <div
-                className="fixed inset-0 bg-black opacity-20 z-[-1]"
-                onClick={() => setIsOpen(false)}
+                    className="fixed inset-0 bg-black opacity-20"
+                    onClick={() => setIsOpen(false)}
                 />
             )}
         </header>
